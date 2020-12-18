a4fd6dcd36316de02f82f0e70742dfb9d37a1567
subinkc <subinkc140@gmail.com>
2020-12-18 14:31:49 -0800

    figuring out

--- app.js
+++ app.js
@@ -1,30 +1,24 @@
 var high=document.getElementById("high");
 var button=document.getElementById("button");
-var numbers=["Question 1","Question 2","Question 2","Question 4",]
-var questions=["What does CSS stand for ?","What do you use to link JS file on hmtl ?",
-"What is purpose of css ?", "How to call a function() ?"]
+var container=document.getElementById("contianer")
+container.textContent="Welcome" //to the quiz. Please choose among 4 options and end of quiz submit with your initials to get score"
+function arr(){
+var  questions=["What does CSS stand for ?","What do you use to link JS file on hmtl ?",
+"What is purpose of css ?", "How to call a function() ?"]}
+
 var answer1=["Cascading style sheet","Contra costa county","function()", "Congress congo can"]
 var answer2=["src","Contra costa county","Community College congress", "Congress congo can"]
 var answer3=["Cascading style sheet","styling","Community College congress", "function()"]
 var answer4=["Cascading style sheet","Contra costa county","Community College congress", "function()"]
 
     function first(){
-        for(var j=0;j<numbers.length;j++){
-       
-            var store=numbers[j]
-
-        }
-        for(var i=0;i<numbers.length;i++){
+        
+        for(var i=0;i<questions.length;i++){
        
             var storel=questions[i]
 
         }
-        document.write(store)
+        document.write(storel)
         }
-
-
-
-
-
-
 button.onclick=first;
+document.getElementById("arr").setAttribute(question,style)
