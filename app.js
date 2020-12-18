f608670ffff32c62c5662cf9d38a2b6a56060e9b
subinkc <subinkc140@gmail.com>
2020-12-17 22:35:45 -0800

    Trying to not kill mysel

---
+++ app.js
@@ -0,0 +1,30 @@
+var high=document.getElementById("high");
+var button=document.getElementById("button");
+var numbers=["Question 1","Question 2","Question 2","Question 4",]
+var questions=["What does CSS stand for ?","What do you use to link JS file on hmtl ?",
+"What is purpose of css ?", "How to call a function() ?"]
+var answer1=["Cascading style sheet","Contra costa county","function()", "Congress congo can"]
+var answer2=["src","Contra costa county","Community College congress", "Congress congo can"]
+var answer3=["Cascading style sheet","styling","Community College congress", "function()"]
+var answer4=["Cascading style sheet","Contra costa county","Community College congress", "function()"]
+
+    function first(){
+        for(var j=0;j<numbers.length;j++){
+       
+            var store=numbers[j]
+
+        }
+        for(var i=0;i<numbers.length;i++){
+       
+            var storel=questions[i]
+
+        }
+        document.write(store)
+        }
+
+
+
+
+
+
+button.onclick=first;
