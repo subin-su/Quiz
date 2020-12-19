567be208fdbae9936dbe01ef18a950b7d0b22eb2
subinkc <subinkc140@gmail.com>
2020-12-19 15:55:57 -0800

    jana

--- app.js
+++ app.js
@@ -1,72 +1,22 @@
-var high=document.getElementById("high");
-var button=document.getElementById("button");
-var container=document.getElementById("container")
-var link=document.getElementById("link")
+var container1=document.getElementById("container1");
+currentquestion=0;
+totalquestions=question.length;
+var question=window.getElementById("question")
+var answer=window.getElementById("answer")
+var option1=window.getElementById("option1")
+var option2=window.getElementById("option2")
+var option3=window.getElementById("option3")
+var option4=window.getElementById("option4")
+function load1(questionIndex){
+    var q=questions[questionIndex]
+    container1.textContent=(questionIndex+1)+"."+q.question
 
-button.addEventListener("click",function(event){
-    event.preventDefault()
-    link()
+}
+load1(currentquestion)
+
+
+
 
     
-})
-container.textContent="Welcome to the quiz. Please choose among 4 options and end of quiz submit with your initials to get score"
 
-let questions= [
-    {
-      id: 1,
-      question: "Who invented computer?",
-      answer: "Charles Babbage",
-      option: [
-        "Thomas Edison",
-        "Albert einstein",
-        "Charles Babbage",
-        "None of the above"
-      ]
-    },
-    {
-      id: 2,
-      question: "When did first program founded?",
-      answer: "1989",
-      option: [
-        "1982",
-        "1980",
-        "1780",
-        "1989"
-      ]
-    },
-    {
-      id: 3,
-      question: "What is the name of first program?",
-      answer: "High-level language",
-      option: [
-        "Machine level language",
-        "javascript",
-        "High-level language",
-        "css"
-      ]
-    },
-    {
-      id: 4,
-      question: "What is the Capital of Usa?",
-      answer: "Washington D.c",
-      option: [
-        "New York",
-        "Washington D.C",
-        "California",
-        "Virginia"
-      ]
-    },
-    {
-      id: 5,
-      question: "Who invented Java?",
-      answer: "James Gosling",
-      option: [
-        "Bill Gates",
-        "Steve Jobs",
-        "Daphne koller",
-        "James Gosling"
-      ]
-    }
-  ]
 
-        
